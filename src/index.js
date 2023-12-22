a11c89e4ae034e360ad9febc99590d791250002c
Florent <florent.grondin2@ent.univ-lorraine.fr>
2023-12-22 15:45:48 +0100

    fix index and install cors to allow only localhost, fix some bug with email and routes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ const express = require('express');
 const server = express();
 const hostname='0.0.0.0';
 const port = 3000;
-
-
 const mongoose =require('mongoose');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: function (origin, callback) {
+      const allowedOrigins = ['http://localhost']; //ajouter autre url
+
+      if (allowedOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+server.use(cors(corsOptions));
+
+
 
 mongoose.connect(process.env.DB_CONNECTION_STRING);
 server.use(express.urlencoded());
