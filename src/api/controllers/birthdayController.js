01236901400826018a9c01ec5cf083f48d7a8251
Florent <florent.grondin2@ent.univ-lorraine.fr>
2023-12-22 11:58:54 +0100

    fix the result of the first method to just disply name and lastname

--- src/api/controllers/birthdayController.js
+++ src/api/controllers/birthdayController.js
@@ -4,7 +4,20 @@ const { DateTime } = require("luxon");
 exports.displayTodayBirthday = async (req, res) => {
   try {
     const todayDate = DateTime.local().startOf('day');
-    const birtdays= await Birthday.find({ date: { $gte: todayDate, $lt: todayDate.plus({ days: 1 }) } });
+    const birtdays= await Birthday.find({ date: { $gte: todayDate, $lt: todayDate.plus({ days: 1 }) } },
+    {_id: false, birthdayDate: false, email: false, created_at: false });
+    res.status(200).json(birtdays);
+  } catch (error) {
+    res.status(500).send("Erreur serveur");
+    console.log(error);
+  }
+};
+
+exports.displayTodayBirthday = async (req, res) => {
+  try {
+    const todayDate = DateTime.local().startOf('day');
+    const birtdays= await Birthday.find({ date: { $gte: todayDate, $lt: todayDate.plus({ days: 1 }) } },
+    {_id: false, birthdayDate: false, email: false, created_at: false });
     res.status(200).json(birtdays);
   } catch (error) {
     res.status(500).send("Erreur serveur");
