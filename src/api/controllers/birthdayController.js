f9705aeb121f73a59b4427a4c84e5d2bd087fc2e
Florent <florent.grondin2@ent.univ-lorraine.fr>
2023-12-22 12:08:41 +0100

    added methode for sending the mail

--- src/api/controllers/birthdayController.js
+++ src/api/controllers/birthdayController.js
@@ -13,12 +13,21 @@ exports.displayTodayBirthday = async (req, res) => {
   }
 };
 
-exports.displayTodayBirthday = async (req, res) => {
+exports.sendMailToday = async (req, res) => {
   try {
     const todayDate = DateTime.local().startOf('day');
-    const birtdays= await Birthday.find({ date: { $gte: todayDate, $lt: todayDate.plus({ days: 1 }) } },
-    {_id: false, birthdayDate: false, email: false, created_at: false });
-    res.status(200).json(birtdays);
+    const birtdays= await Birthday.find({ date: { $gte: todayDate, $lt: todayDate.plus({ days: 1 }) } });
+    if (birtdays.length > 0) {
+      birtdays.forEach((birtdayPerson) => {
+        console.log("Mail de l'utilisateur :", birtdayPerson.mail);
+        res.status(200).json("mail envoyé");
+      });
+    } else {
+      console.error("Aucun utilisateur trouvé pour la date d'aujourd'hui.");
+      return res.status(404).send("Aucun utilisateur trouvé pour la date d'aujourd'hui.");
+    }
+    
+    
   } catch (error) {
     res.status(500).send("Erreur serveur");
     console.log(error);
