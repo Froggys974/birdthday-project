5f81d42c8e6f4a75e9c786976aec6a80d7e853c1
Florent <florent.grondin2@ent.univ-lorraine.fr>
2023-12-23 01:47:00 +0100

    modified and fixes some bug on index

--- src/index.js
+++ src/index.js
@@ -3,14 +3,35 @@ const express = require('express');
 const server = express();
 const hostname='0.0.0.0';
 const port = 3000;
+const mongoose =require('mongoose');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: function (origin, callback) {
+      const allowedOrigins = ['http://localhost']; //ajouter autre url
+
+      if (allowedOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+server.use(cors(corsOptions));
 
 
 
+mongoose.connect(process.env.DB_CONNECTION_STRING);
 server.use(express.urlencoded());
+
 server.use(express.json());
 
-const postRoute=require('../../testNode2/src/api/routes/quotesRoute');
-postRoute(server);
+const birthdayRoute=require('./api/routes/birthdayRoute');
+birthdayRoute(server);
 
 
 
